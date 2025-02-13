7f8377eb3ccec968ad7c3fa23797d75688e40415
Amol Shirke <shirkeamy@gmail.com>
2025-02-13 07:36:34 +0530

    Session 1 - introduction to typescript

---
+++ typescript/Demo.js
@@ -0,0 +1,4 @@
+var a = "Hello World";
+console.log(a);
+var num1 = 10;
+console.log(num1);
---
+++ typescript/Demo.ts
@@ -0,0 +1,5 @@
+const a: string = "Hello World";
+console.log(a);
+
+const num1: number = 10;
+console.log(num1);
