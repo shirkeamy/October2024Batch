10a135af1fe23a583202bbd2a47d9cbbfc3fc08f
Amol Shirke <shirkeamy@gmail.com>
2025-02-13 09:12:25 +0530

    js after compile

--- typescript/Demo.js
+++ typescript/Demo.js
@@ -1,8 +1,8 @@
 var numberVar = 20;
 console.log(numberVar);
-var a = "Hello World";
-console.log(a);
 document.addEventListener("DOMContentLoaded", function () {
+    var a = "Hello World";
+    console.log(a);
     var heading = document.createElement("h2");
     heading.textContent = a;
     document.body.appendChild(heading);
