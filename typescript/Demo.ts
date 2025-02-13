024ab7fd9d401179c07cbb2e20bf302806ed55dc
Amol Shirke <shirkeamy@gmail.com>
2025-02-13 09:12:04 +0530

    Refactor code and shift variable inside listner

--- typescript/Demo.ts
+++ typescript/Demo.ts
@@ -3,15 +3,13 @@
 const numberVar: number = 20;
 console.log(numberVar);
 
-const a: string = "Hello World";
-console.log(a);
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const a: string = "Hello World";
+    console.log(a);
     let heading = document.createElement("h2");
-
     heading.textContent = a;
-
     document.body.appendChild(heading);
 
 })
