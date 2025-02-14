1d95bd6624844f99e8ef369614c55e38e264f6a7
Amol Shirke <shirkeamy@gmail.com>
2025-02-14 08:58:03 +0530

    Delete demo js

--- typescript/Demo.js
+++
@@ -1,9 +0,0 @@
-var numberVar = 20;
-console.log(numberVar);
-document.addEventListener("DOMContentLoaded", function () {
-    var a = "Hello World";
-    console.log(a);
-    var heading = document.createElement("h2");
-    heading.textContent = a;
-    document.body.appendChild(heading);
-});
