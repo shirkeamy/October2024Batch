334caf802f6c4e377d86f8438712febbe5d60c2a
Amol Shirke <shirkeamy@gmail.com>
2025-02-14 08:58:12 +0530

    add app ts

---
+++ typescript/App.ts
@@ -0,0 +1,37 @@
+// let
+// modify value from variable
+// no need to add value when declare
+
+// cosnt
+// can not modify value from variable
+// need to add value when declare
+
+let varName: string;
+varName = "Any string";
+
+let Name = true;
+
+// 'const' declarations must be initialized.
+const varName1: string = "";
+const boolVar: boolean = true;
+const marks = true;
+
+let EmployeeDetail: {
+    name: string;
+    age: number;
+    salary: number;
+    isActive: boolean;
+    dob: Date;
+}
+
+EmployeeDetail = {
+    name: "John Doe",
+    age: 45,
+    salary: 15000,
+    isActive: true,
+    dob: new Date()
+}
+
+console.log(EmployeeDetail)
+console.log(EmployeeDetail.name)
+console.log(EmployeeDetail.dob)
