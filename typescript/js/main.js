a6ee524c8eeb87ddedc3c871fdb9d0d05f0c236c
Amol Shirke <shirkeamy@gmail.com>
2025-02-14 07:52:22 +0530

    create combined file using tsc --outFile ./js/main.js Demo.ts app.ts emp.ts

---
+++ typescript/js/main.js
@@ -0,0 +1,17 @@
+var numberVar = 20;
+console.log(numberVar);
+document.addEventListener("DOMContentLoaded", function () {
+    var a = "Hello World";
+    console.log(a);
+    var heading = document.createElement("h2");
+    heading.textContent = a;
+    document.body.appendChild(heading);
+});
+// End of demo ts
+console.log('app.ts file');
+var Addition = function (a, b) {
+    return a + b;
+};
+// End of app ts
+console.log('Employee');
+// End of emp ts
