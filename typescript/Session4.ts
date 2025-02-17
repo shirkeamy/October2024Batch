12079659ac902e2f3079de609ee334423994c32f
Amol Shirke <shirkeamy@gmail.com>
2025-02-17 08:53:39 +0530

    add if

---
+++ typescript/Session4.ts
@@ -0,0 +1,10 @@
+let number1: number = 12;
+let number2: number = 12;
+
+if (number1 > number2) {
+    console.log("number1 is greater than number2")
+} else if (number1 === number2) {
+    console.log("number1 is eqal number2")
+} else {
+    console.log("number1 is less than number2")
+}
