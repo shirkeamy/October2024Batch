5657b3d10b76e4d0be0cde348978cddd1d54eb51
Amol Shirke <shirkeamy@gmail.com>
2025-02-17 08:53:58 +0530

    If from UI

--- typescript/Session4.ts
+++ typescript/Session4.ts
@@ -8,3 +8,25 @@ if (number1 > number2) {
 } else {
     console.log("number1 is less than number2")
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let number1 = document.createElement("input")
+    let number2 = document.createElement("input")
+    let checkButton = document.createElement("button")
+    checkButton.innerText = "Check values";
+    checkButton.addEventListener("click", () => {
+        if (number1.value > number2.value) {
+            console.log("number1 is greater than number2")
+        } else if (number1.value === number2.value) {
+            console.log("number1 is eqal number2")
+        } else {
+            console.log("number1 is less than number2")
+        }
+    })
+
+    document.body.appendChild(number1)
+    document.body.appendChild(number2)
+    document.body.appendChild(checkButton)
+
+});
