6909a41aa38213e7a82314e76eebb768510831a9
Amol Shirke <shirkeamy@gmail.com>
2025-02-17 08:54:15 +0530

    switch case

--- typescript/Session4.ts
+++ typescript/Session4.ts
@@ -30,3 +30,40 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(checkButton)
 
 });
+
+///////////////
+// Switch case
+///////////////
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let heading = document.createElement("h1")
+    heading.textContent = "Switch Case"
+    let textBox = document.createElement("input")
+    let para = document.createElement("p")
+    let checkButton = document.createElement("button")
+    checkButton.innerText = "Check grade";
+    checkButton.addEventListener("click", () => {
+        switch (textBox.value) {
+            case "A":
+                para.textContent = "Excellent"
+                break;
+            case "B":
+                para.textContent = "Good"
+                break;
+            case "C":
+                para.textContent = "Fair"
+                break;
+            default:
+                para.textContent = "Invalid Choise!"
+                break;
+        }
+    })
+
+    document.body.appendChild(heading)
+    document.body.appendChild(textBox)
+    document.body.appendChild(checkButton)
+    document.body.appendChild(para)
+
+});
+
