9403e5e79c1d73fcf0a24e4ef71b551e7ac10b66
Amol Shirke <shirkeamy@gmail.com>
2025-02-17 07:48:32 +0530

    if else from UI

--- typescript/ControlStatements.ts
+++ typescript/ControlStatements.ts
@@ -10,3 +10,23 @@ else if (number1 === number2){
     console.log("number2 is greater than number2");
 
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let number1 = document.createElement("input");
+    let number2 = document.createElement("input");
+    let checkButton = document.createElement("button")
+    checkButton.innerText = "Check";
+    checkButton.addEventListener("click", () => {
+        if (number1.value > number2.value) {
+            console.info(`number1: ${number1.value} is greater than number2: ${number2.value}`)
+        }
+        else if (number1.value === number2.value) {
+            console.info(`number1: ${number1.value} is same as number2: ${number2.value}`)
+        } else {
+            console.info(`number1: ${number1.value} is less than number2: ${number2.value}`)
+        }
+    });
+    document.body.appendChild(number1)
+    document.body.appendChild(number2)
+    document.body.appendChild(checkButton);
+})
