bc7803ef0fea914012fd6932dead8febba9bac22
Amol Shirke <shirkeamy@gmail.com>
2025-02-17 07:43:51 +0530

    if else

--- typescript/ControlStatements.ts
+++ typescript/ControlStatements.ts
@@ -1,7 +1,9 @@
 let number1: number = 12;
-let number2: number = 10;
+let number2: number = 50;
 
 if(number1 > number2){
     console.log("number1 is greater than number2");
 }
-
+else{
+    console.log("number2 is greater than number1");
+}
