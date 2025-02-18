1d3a89252f8e21b2053fd960cdf7299dfaaa0eb0
Amol Shirke <shirkeamy@gmail.com>
2025-02-18 07:23:27 +0530

    Factorial of given number in for

--- typescript/forloop.ts
+++ typescript/forloop.ts
@@ -8,3 +8,12 @@ let tableFor: number = 10;
 for (let i = 1; i <= 10; i++) {
     console.log(tableFor * i);
 }
+
+console.log("3. Factorial of given number")
+
+let factFor: number = 3;
+let factorial: number = 1;
+for (let i = 1; i <= factFor; i++) {
+    factorial = factorial * i
+}
+console.log(factorial);
