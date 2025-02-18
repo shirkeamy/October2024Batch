128f23a69a7bff9d72a2ecec7091cff0037cc5fa
Amol Shirke <shirkeamy@gmail.com>
2025-02-18 07:23:14 +0530

    table of given number in for

--- typescript/forloop.ts
+++ typescript/forloop.ts
@@ -2,3 +2,9 @@ console.log("1. Print 1 to 10 numbers")
 for (let i = 0; i < 10; i++) {
     console.log(i + 1);
 }
+
+console.log("2. table of given number")
+let tableFor: number = 10;
+for (let i = 1; i <= 10; i++) {
+    console.log(tableFor * i);
+}
