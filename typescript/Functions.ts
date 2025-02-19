5e46009d332c3460cdc027c5b5be1b46c6bda7d4
Amol Shirke <shirkeamy@gmail.com>
2025-02-19 08:01:50 +0530

    Function way 1 normal function

---
+++ typescript/Functions.ts
@@ -0,0 +1,10 @@
+/////////
+// way 1 - normal function
+////////
+function addition(a: number, b: number): number {
+    return a + b
+}
+
+let res: number = addition(1, 2);
+
+console.log(`Addition of given number is ${res}`);
