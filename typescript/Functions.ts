0f4c4feca974813b076bb3813fb689cb97c406df
Amol Shirke <shirkeamy@gmail.com>
2025-02-19 08:02:12 +0530

    Way 2 variable as a function

--- typescript/Functions.ts
+++ typescript/Functions.ts
@@ -8,3 +8,19 @@ function addition(a: number, b: number): number {
 let res: number = addition(1, 2);
 
 console.log(`Addition of given number is ${res}`);
+
+/////////
+// way 2 - variable as a function
+////////
+
+let division = function (a: number, b: number) {
+
+    if(b === 0){
+        console.warn("You can not divide any number with 0")
+        return 0;
+    }
+
+    return a/b;
+}
+
+console.log(`Division of given number is ${division(1,2)}`);
