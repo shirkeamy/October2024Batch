efab52c03a913a0300c146b290832af3ad99e587
Amol Shirke <shirkeamy@gmail.com>
2025-02-21 08:53:54 +0530

    Arrow function without type

--- typescript/Session6.ts
+++ typescript/Session6.ts
@@ -12,3 +12,12 @@ const division = function (num1: number, num2: number) {
 }
 
 console.log(`Division of ${10} and ${5} is ${division(10, 5)}`)
+
+// const fnName = () => {}
+
+const multiplication = (num1: number, num2: number) => {
+    return num1 * num2;
+}
+
+console.log(`multiplication of ${10} and ${5} is ${multiplication(10, 5)}`);
+
