bd6532983f0dbc18d2ac7b7a9caa451f9f62a6b0
Amol Shirke <shirkeamy@gmail.com>
2025-02-21 08:58:09 +0530

    add division funtion in UI

--- typescript/Session6.ts
+++ typescript/Session6.ts
@@ -60,11 +60,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
         div.textContent = result.toString();
     })
 
+    divButton.addEventListener("click",()=>{
+        let result: number = division(parseInt(num1.value), parseInt(num2.value))
+        div.textContent = result.toString();
+    })
+
     document.body.appendChild(num1)
     document.body.appendChild(num2)
     document.body.appendChild(addButton)
     document.body.appendChild(subButton)
     document.body.appendChild(MultButton)
+    document.body.appendChild(divButton)
     document.body.appendChild(div)
 
 });
