a1fe96c659645101f4a704b7e11a267cd626d3d7
Amol Shirke <shirkeamy@gmail.com>
2025-03-01 07:45:16 +0530

    Add class component and render it

---
+++ react-test/my-app/src/Pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>Home Page</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas aperiam minus explicabo laboriosam nemo, impedit eum quidem accusamus ad praesentium harum suscipit quos sequi inventore est. Quae, ut voluptatibus?
+                </p>
+            </>
+        )
+    }
+
+}
+
+export default Home;
