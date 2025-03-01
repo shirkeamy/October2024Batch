391be946c33e9ae7a9447c0845ffa8da47f1139a
Amol Shirke <shirkeamy@gmail.com>
2025-03-01 07:51:25 +0530

    Add function component 2 - using React.FC

--- react-test/my-app/src/App.tsx
+++ react-test/my-app/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <>
       <Home />
       <About />
+      <Contact />
     </>
   );
 }
---
+++ react-test/my-app/src/Pages/Contact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact: React.FC = () => {
+    return (
+        <>
+            <h1>Contact page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, omnis repellendus. Aut molestias accusantium sit accusamus at fuga perspiciatis expedita quidem, repudiandae distinctio ullam et dolorum quam sint fugit dolorem?
+            </p>
+            <h3>Lorem, ipsum.</h3>
+        </>
+    )
+}
+
+export default Contact;
