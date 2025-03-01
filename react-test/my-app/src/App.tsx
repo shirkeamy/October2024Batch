af9c007024f21f622cbffa9e657a4e89278305dd
Amol Shirke <shirkeamy@gmail.com>
2025-03-01 07:47:40 +0530

    Add function component - 1

--- react-test/my-app/src/App.tsx
+++ react-test/my-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 }
---
+++ react-test/my-app/src/Pages/About.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+function About() {
+    return (
+        <>
+            <h1>
+                About Page
+            </h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, eaque.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia repellat explicabo placeat iusto, consectetur at quas hic minima fugit sapiente.
+            </p>
+            <ol>
+                <li>List item 1</li>
+                <li>List item 2</li>
+                <li>List item 3</li>
+            </ol>
+        </>
+
+    )
+}
+
+export default About;
