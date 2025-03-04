187c4e3cf318399317b6118a9211b53508ad2134
Amol Shirke <shirkeamy@gmail.com>
2025-03-04 22:14:47 +0530

    revert previous commit

--- react-test/my-app/src/Pages/User.tsx
+++ react-test/my-app/src/Pages/User.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import ComponentA from "./Components/ComponentA";
 
-const User: React.FC = () => {
+interface IUserProps {
+    userName: string;
+}
+
+const User: React.FC<IUserProps> = (props: IUserProps) => {
+
+    const { userName }: IUserProps = props;
+    
+    const email: string = localStorage.getItem("EmailId") ?? "";
+    const password: string = localStorage.getItem("Password") ?? "";
 
     return (
         <>
-            
+            <h1>User page</h1>
+            <h3>Welcome, {userName}</h3>
+            <p>
+                your email id is {email}
+                <br />
+                your password is {password}
+            </p>
+            <p>
+                below is the Component A
+            </p>
+            <ComponentA userName={userName} />
         </>
     )
 }
