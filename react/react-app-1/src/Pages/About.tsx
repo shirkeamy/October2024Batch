802fc02796c7c672bb06db0bb6a1df95e94f778c
Amol Shirke <shirkeamy@gmail.com>
2025-03-06 20:36:15 +0530

    Student - Session 6

--- react/react-app-1/src/Pages/About.tsx
+++ react/react-app-1/src/Pages/About.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function About() {
+
+    const [count, setCount] = useState<number>(0);
+
     return (
         <>
             <h1>About Page</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, non.
             </p>
+
+            <h3>Count is {count}</h3>
+            <button onClick={()=>{setCount(count+1)}}>Add value</button>
         </>
     )
 }
--- react/react-app-1/src/Pages/Contact.tsx
+++ react/react-app-1/src/Pages/Contact.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact: React.FC = () => {
+    const [userName, setUserName] = useState("");
+    const [emailId, setEmailId] = useState<string>("");
     return (
         <>
             <h1>Contact Page</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus nostrum eveniet necessitatibus perspiciatis quisquam debitis culpa similique praesentium consequatur officia nesciunt hic iusto laboriosam excepturi, voluptates et rerum molestias pariatur.
             </p>
+
+            <div className="container">
+                <h3>
+                    User Name - {userName} <br />
+                    Email id - {emailId}
+                </h3>
+                <div className="row">
+                    <div className="col-md-6">
+                        <input type="text" name="" id="txtUserName"
+                            className='form-control'
+                            value={userName}
+                            onChange={(event) => {
+                                let { value } = event.target
+                                setUserName(value);
+                            }}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <input type="text" name="" id="txtEmail" className='form-control'
+                            value={emailId}
+                            onChange={(event) => {
+                                let { value } = event.target
+                                setEmailId(value);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
