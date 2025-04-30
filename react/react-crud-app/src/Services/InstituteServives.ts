738ef2c885330d7ba6de80094ccd3bcc08d45276
Amol Shirke <shirkeamy@gmail.com>
2025-04-30 21:09:21 +0530

    Add subject management functions to InstituteServices

--- react/react-crud-app/src/Services/InstituteServives.ts
+++ react/react-crud-app/src/Services/InstituteServives.ts
@@ -1,4 +1,4 @@
-import { IApiResponse, ICourse } from "../Utils/Interfaces";
+import { IApiResponse, ICourse, ISubject, ISubjectPostData } from "../Utils/Interfaces";
 
 export const GetCourses = async (): Promise<ICourse[]> => {
     const response = await fetch(`https://localhost:7164/api/courses`)
@@ -71,6 +71,85 @@ export const DeleteCourse = async (courseId: number): Promise<boolean> => {
 
     if (response.ok) {
         const data: IApiResponse<ICourse> = await response.json();
+        return data.success;
+    } else {
+        return false;
+    }
+}
+
+
+export const GetSubjests = async (): Promise<ISubject[]> => {
+    const response = await fetch(`https://localhost:7164/api/subjests`)
+
+    if (response.ok) {
+        const data: IApiResponse<ISubject[]> = await response.json();
+        if (data.success)
+            return data.payload;
+        else
+            return [];
+    } else {
+        return [];
+    }
+
+}
+
+
+export const GetSubjectById = async (subjectId: number): Promise<ISubject> => {
+    const response = await fetch(`https://localhost:7164/api/subjests/${subjectId}`)
+
+    if (response.ok) {
+        const data: IApiResponse<ISubject> = await response.json();
+        if (data.success)
+            return data.payload;
+        else
+            return {} as ISubject;
+    } else {
+        return  {} as ISubject;
+    }
+
+}
+
+export const SaveSubject = async (subject: ISubjectPostData): Promise<boolean> => {
+    const response = await fetch(`https://localhost:7164/api/subjests`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(subject)
+    });
+
+    if (response.ok) {
+        const data: IApiResponse<ISubjectPostData> = await response.json();
+        return data.success;
+    } else {
+        return false;
+    }
+}
+
+export const UpdateSubject = async (subjectId: number, subject: ISubjectPostData): Promise<boolean> => {
+    const response = await fetch(`https://localhost:7164/api/subjests/${subjectId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subject)
+    });
+
+    if (response.ok) {
+        const data: IApiResponse<ISubjectPostData> = await response.json();
+        return data.success;
+    } else {
+        return false;
+    }
+}
+
+export const DeleteSubject = async (subjectId: number): Promise<boolean> => {
+    const response = await fetch(`https://localhost:7164/api/subjests/${subjectId}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        const data: IApiResponse<ISubject> = await response.json();
         return data.success;
     } else {
         return false;
